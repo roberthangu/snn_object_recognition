d54451552d29fd0a13d2c3f1f22fc2006fe5adcf
Robert Hangu <robert.hangu@gmail.com>
2016-08-21 15:50:06 +0200

    Add commandline arguments inside of the scripts

--- c1-spikes-from-file-test.py
+++ c1-spikes-from-file-test.py
@@ -6,19 +6,32 @@ import pyNN.nest as sim
 import pathlib as plb
 import time
 import pickle
+import argparse as ap
 
 import common as cm
 import network as nw
 import visualization as vis
 import time
 
-args = cm.parse_args()
+parser = ap.ArgumentParser('./c1-spikes-from-file-test.py --')
+parser.add_argument('--c1-dumpfile', type=str, required=True,
+                    help='The output file to contain the C1 spiketrains')
+parser.add_argument('--dataset-label', type=str, required=True,
+                    help='The name of the dataset which was used for\
+                    training')
+parser.add_argument('--image-count', type=int, required=True,
+                    help='The number of images to read from the training\
+                    directory')
+parser.add_argument('--refrac-s2', type=float, default=.1, metavar='.1',
+                    help='The refractory period of neurons in the S2 layer in\
+                    ms')
+parser.add_argument('--sim-time', default=50, type=float, metavar='50',
+                     help='Simulation time')
+args = parser.parse_args()
 
 sim.setup(threads=4)
 
 layer_collection = {}
-
-target_img = cv2.imread(args.target_name, cv2.CV_8UC1)
 
 # Read the gabor features for reconstruction
 feature_imgs_dict = {} # feature string -> image
@@ -38,8 +51,8 @@ for size, layers_as_dicts in ddict.items():
         spiketrains = layer_as_dict['segment'].spiketrains
         dimensionless_sts = [[s for s in st] for st in spiketrains]
         new_layer = nw.Layer(sim.Population(n * m,
-                        sim.SpikeSourceArray(spike_times=dimensionless_sts)), (n, m))
-        new_layer.population.label = layer_as_dict['label']
+                        sim.SpikeSourceArray(spike_times=dimensionless_sts),
+                        label=layer_as_dict['label']), (n, m))
         layer_list.append(new_layer)
     layer_collection['C1'][size] = layer_list
 print('C1 creation took {} s'.format(time.clock() - t1))
@@ -50,30 +63,29 @@ layer_collection['S2'] = nw.create_S2_layers(layer_collection['C1'], args)
 print('S2 creation took {} s'.format(time.clock() - t1))
 #create_S2_inhibition(layer_collection['S2'])
 
-for layer_name in ['C1']:
-    if layer_name in layer_collection:
-        for layers in layer_collection[layer_name].values():
-            for layer in layers:
-                layer.population.record('spikes')
-for layer in layer_collection['S2'].values():
-    layer.population.record(['spikes', 'v'])
+#for layer_name in ['C1']:
+#    if layer_name in layer_collection:
+#        for layers in layer_collection[layer_name].values():
+#            for layer in layers:
+#                layer.population.record('spikes')
+#for layer in layer_collection['S2'].values():
+#    layer.population.record(['spikes', 'v'])
 
 print('========= Start simulation =========')
 start_time = time.clock()
-sim.run(args.sim_time)
+for i in range(args.image_count):
+    sim.run(args.sim_time)
+    vis.plot_C1_spikes(layer_collection['C1'],
+                       '{}_image_{}'.format(args.dataset_label, i))
+    vis.plot_S2_spikes(layer_collection['S2'], 
+                       '{}_image_{}'.format(args.dataset_label, i))
+    updated_weights = nw.update_shared_weights(layer_collection['S2'])
+#    if (i + 1) % 100 == 0:
+    cv2.imwrite('S2_reconstructions/{}_{}'.format(args.dataset_label, i),
+                vis.reconstruct_S2_features(updated_weights,
+                                            feature_imgs_dict))
 end_time = time.clock()
 print('========= Stop  simulation =========')
 print('Simulation took', end_time - start_time, 's')
-
-final_weights = nw.update_shared_weights(layer_collection['S2'])
-reconstruction = vis.reconstruct_S2_features(final_weights, feature_imgs_dict)
-
-cv2.imwrite('S2_reconstructions/{}.png'.format(plb.Path(args.target_name).stem),
-            reconstruction)
-
-t1 = time.clock()
-print('Plotting spikes')
-vis.plot_spikes(layer_collection, args)
-print('Plotting spiketrains took {} s'.format(time.clock() - t1))
 
 sim.end()
--- dump-c1-spikes.py
+++ dump-c1-spikes.py
@@ -6,23 +6,43 @@ import pyNN.nest as sim
 import pathlib as plb
 import time
 import pickle
+import argparse as ap
 
-import common as cm
 import network as nw
 import visualization as vis
-import time
 
-args = cm.parse_args()
+parser = ap.ArgumentParser('./dump-c1-spikes.py --')
+parser.add_argument('--dataset-label', type=str, required=True,
+                    help='The name of the dataset which was used for\
+                    training')
+parser.add_argument('--image-count', type=int, required=True,
+                    help='The number of images to read from the training\
+                    directory')
+parser.add_argument('--training-dir', type=str, required=True,
+                    help='The directory with the training images')
+parser.add_argument('--refrac-c1', type=float, default=.1, metavar='0.1',
+                    help='The refractory period of neurons in the C1 layer in\
+                    ms')
+parser.add_argument('--sim-time', default=50, type=float, help='Simulation time',
+                    metavar='50')
+parser.add_argument('--scales', default=[1.0, 0.71, 0.5, 0.35, 0.25],
+                    nargs='+', type=float,
+                    help='A list of image scales for which to create\
+                    layers. Defaults to [1, 0.71, 0.5, 0.35, 0.25]')
+args = parser.parse_args()
+
+training_path = plb.Path(args.training_dir)
+imgs = [(filename.stem, cv2.imread(filename.as_posix(), cv2.CV_8UC1))\
+            for filename in training_path.iterdir()]
 
 sim.setup(threads=4)
 
 layer_collection = {}
 
-target_img = cv2.imread(args.target_name, cv2.CV_8UC1)
 print('Create S1 layers')
 t1 = time.clock()
 layer_collection['S1'] =\
-    nw.create_gabor_input_layers_for_scales(target_img, args.scales)
+    nw.create_empty_input_layers_for_scales(imgs[0][1], args.scales)
 nw.create_cross_layer_inhibition(layer_collection['S1'])
 print('S1 layer creation took {} s'.format(time.clock() - t1))
 
@@ -41,20 +61,26 @@ for layer_name in ['C1']:
 
 print('========= Start simulation =========')
 start_time = time.clock()
-sim.run(args.sim_time)
+count = 0
+for filename, target_img in imgs[0:args.image_count]:
+    print('Simulating for', filename, 'number', count)
+    count += 1
+    nw.set_i_offsets_for_all_scales_to(layer_collection['S1'], target_img)
+    sim.run(args.sim_time)
 end_time = time.clock()
 print('========= Stop  simulation =========')
 print('Simulation took', end_time - start_time, 's')
 
 print('Dumping spikes for all scales and layers')
 ddict = {}
-filename = 'C1_spike_data/{}'.format(plb.Path(args.target_name).stem)
+filename = 'C1_spike_data/' + args.dataset_label
 for size, layers in layer_collection['C1'].items():
     ddict[size] = [{'segment': layer.population.get_data().segments[0],
                     'shape': layer.shape,
                     'label': layer.population.label } for layer in layers]
     filename += '_{}'.format(size)
-dumpfile = open('{}_{}ms.bin'.format(filename, args.sim_time), 'wb')
+dumpfile = open('{}_{}ms_{}_images.bin'.format(filename, args.sim_time,
+                                               args.image_count), 'wb')
 pickle.dump(ddict, dumpfile, protocol=4)
 dumpfile.close()
 
