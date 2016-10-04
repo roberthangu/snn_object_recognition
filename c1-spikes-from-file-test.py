36a92c5541d545f4d2055c0b72f9127a8c04fc30
Robert Hangu <robert.hangu@gmail.com>
2016-10-04 15:51:49 +0200

    Remove MPI code and add epoch length commandline parameter

--- c1-spikes-from-file-test.py
+++ c1-spikes-from-file-test.py
@@ -26,6 +26,8 @@ parser.add_argument('--c1-dumpfile', type=str, required=True,
 parser.add_argument('--dataset-label', type=str, required=True,
                     help='The name of the dataset which was used for\
                     training')
+parser.add_argument('--epoch', type=int, default=30,
+                    help='The lenght of an epoch')
 parser.add_argument('--feature-size', type=int, default=3,
                      help='The size of the features to be learnt')
 parser.add_argument('--s2-prototype-cells', type=int, default=3,
@@ -61,11 +63,6 @@ signal.signal(signal.SIGBUS, handler)
 signal.signal(signal.SIGILL, handler)
 signal.signal(signal.SIGSEGV, handler)
 
-MPI_ROOT = 0
-
-def is_root():
-    return MPI.COMM_WORLD.rank == MPI_ROOT 
-
 sim.setup(threads=args.threads, min_delay=.1)
 
 layer_collection = {}
@@ -80,9 +77,8 @@ dataset_label = '{}_fs{}_{}imgs_{}ms_scales'.format(args.dataset_label,
                                         args.feature_size, args.image_count,
                                         int(args.sim_time))
 
-if is_root():
-    print('Create C1 layers')
-    t1 = time.clock()
+print('Create C1 layers')
+t1 = time.clock()
 dumpfile = open(args.c1_dumpfile, 'rb')
 ddict = pickle.load(dumpfile)
 layer_collection['C1'] = {}
@@ -98,15 +94,12 @@ for size, layers_as_dicts in ddict.items():
                         label=layer_as_dict['label']), (n, m))
         layer_list.append(new_layer)
     layer_collection['C1'][size] = layer_list
-if is_root():
-    print('C1 creation took {} s'.format(time.clock() - t1))
+print('C1 creation took {} s'.format(time.clock() - t1))
 
-if is_root():
-    print('Creating S2 layers')
-    t1 = time.clock()
+print('Creating S2 layers')
+t1 = time.clock()
 layer_collection['S2'] = nw.create_S2_layers(layer_collection['C1'], args)
-if is_root():
-    print('S2 creation took {} s'.format(time.clock() - t1))
+print('S2 creation took {} s'.format(time.clock() - t1))
 
 for layers in layer_collection['C1'].values():
     for layer in layers:
@@ -115,60 +108,54 @@ for layer_list in layer_collection['S2'].values():
     for layer in layer_list:
         layer.population.record(['spikes', 'v'])
 
-if is_root():
-    reconstructions_dir_dataset_path = plb.Path('S2_reconstructions/' + dataset_label)
-    if not reconstructions_dir_dataset_path.exists():
-        reconstructions_dir_dataset_path.mkdir(parents=True)
-    c1_plots_dir_path = plb.Path('plots/C1/' + dataset_label)
-    if not c1_plots_dir_path.exists():
-        c1_plots_dir_path.mkdir(parents=True)
-    s2_plots_dataset_dir = plb.Path('plots/S2/' + dataset_label)
-    for i in range(args.s2_prototype_cells):
-        s2_plots_dir_path = s2_plots_dataset_dir / str(i)
-        if not s2_plots_dir_path.exists():
-            s2_plots_dir_path.mkdir(parents=True)
+reconstructions_dir_dataset_path = plb.Path('S2_reconstructions/' + dataset_label)
+if not reconstructions_dir_dataset_path.exists():
+    reconstructions_dir_dataset_path.mkdir(parents=True)
+c1_plots_dir_path = plb.Path('plots/C1/' + dataset_label)
+if not c1_plots_dir_path.exists():
+    c1_plots_dir_path.mkdir(parents=True)
+s2_plots_dataset_dir = plb.Path('plots/S2/' + dataset_label)
+for i in range(args.s2_prototype_cells):
+    s2_plots_dir_path = s2_plots_dataset_dir / str(i)
+    if not s2_plots_dir_path.exists():
+        s2_plots_dir_path.mkdir(parents=True)
 
 out_dumpfile = open(args.weights_to, 'wb')
 
-if is_root():
-    print('========= Start simulation =========')
-    start_time = time.clock()
+print('========= Start simulation =========')
+start_time = time.clock()
 for i in range(args.image_count):
-    if is_root():
-        print('Simulating for image number', i)
+    print('Simulating for image number', i)
     sim.run(args.sim_time)
-    if is_root():
-        if args.plot_c1_spikes:
-            vis.plot_C1_spikes(layer_collection['C1'],
-                               '{}_image_{}'.format(dataset_label, i),
-                               out_dir_name=c1_plots_dir_path.as_posix())
-        if args.plot_s2_spikes:
-            vis.plot_S2_spikes(layer_collection['S2'],
+    if args.plot_c1_spikes:
+        vis.plot_C1_spikes(layer_collection['C1'],
                            '{}_image_{}'.format(dataset_label, i),
-                           args.s2_prototype_cells,
-                           out_dir_name=s2_plots_dataset_dir.as_posix())
-    if is_root():
-        if (i + 1) % 10 == 0:
-            current_weights = nw.get_current_weights(layer_collection['S2'],
-                                                     args.s2_prototype_cells)
-            cv2.imwrite('{}/{}_{}_images.png'.format(\
-                     reconstructions_dir_dataset_path.as_posix(),
-                     dataset_label, i + 1),
-                vis.reconstruct_S2_features(current_weights,
-                                            feature_imgs_dict,
-                                            args.feature_size))
-if is_root():
-    end_time = time.clock()
-    print('========= Stop  simulation =========')
-    print('Simulation took', end_time - start_time, 's')
-    # Reconstruct the last image
-    cv2.imwrite('{}/{}_{}_images.png'.format(\
-             reconstructions_dir_dataset_path.as_posix(),
-             dataset_label, i + 1),
-        vis.reconstruct_S2_features(current_weights,
-                                    feature_imgs_dict,
-                                    args.feature_size))
-    print('Dumping trained weights to file', args.weights_to)
-    pickle.dump(current_weights, out_dumpfile)
+                           out_dir_name=c1_plots_dir_path.as_posix())
+    if args.plot_s2_spikes:
+        vis.plot_S2_spikes(layer_collection['S2'],
+                       '{}_image_{}'.format(dataset_label, i),
+                       args.s2_prototype_cells,
+                       out_dir_name=s2_plots_dataset_dir.as_posix())
+    if (i + 1) % 10 == 0:
+        current_weights = nw.get_current_weights(layer_collection['S2'],
+                                                 args.s2_prototype_cells)
+        cv2.imwrite('{}/{}_{}_images.png'.format(\
+                 reconstructions_dir_dataset_path.as_posix(),
+                 dataset_label, i + 1),
+            vis.reconstruct_S2_features(current_weights,
+                                        feature_imgs_dict,
+                                        args.feature_size))
+end_time = time.clock()
+print('========= Stop  simulation =========')
+print('Simulation took', end_time - start_time, 's')
+# Reconstruct the last image
+cv2.imwrite('{}/{}_{}_images.png'.format(\
+         reconstructions_dir_dataset_path.as_posix(),
+         dataset_label, i + 1),
+    vis.reconstruct_S2_features(current_weights,
+                                feature_imgs_dict,
+                                args.feature_size))
+print('Dumping trained weights to file', args.weights_to)
+pickle.dump(current_weights, out_dumpfile)
 
 sim.end()
